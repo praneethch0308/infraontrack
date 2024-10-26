6acfe299b9b5db7a4a6066fd2b247b9ec092e2e5
Sai Praneeth <praneethchilukuri03@gmail.com>
2024-10-26 16:22:04 +0530

    asset-create

--- src/pages/assets/AssetCreate.jsx
+++ src/pages/assets/AssetCreate.jsx
@@ -8,46 +8,52 @@ import Image from '@mui/icons-material/Image';
 import Sidebar from '../../components/Sidebar';
 import { useDept } from '../../context/departments/DeptContext';
 import EmployeeContext from '../../context/employees/EmployeeContext';
+import axios from 'axios';
+import { useList } from '../../context/lists/ListContext';
+import { useLoctn } from '../../context/locations/LocationContext';
+import { useVendor } from '../../context/vendors/VendorContext';
 
 const AssetCreate = () => {
   const navigate = useNavigate();
-  const [formValues, setFormValues] = useState({ 
-      assetName:'' ,
-      make: '',
-      model: '',
-      isSerialized: false,
-      serialNo: '',
-      description: '',
-      geoLocation: '', 
-      purchaseDate: '', 
-      warranty: '',
-      isWarrantyExpired: false,
-      isUnderWarranty: false,
-      status: '',
-      isApprovedByHOD: false,
-      isApprovedByPrinicipal: false,
-      deptInfoDto: {
-          id: ''
-          
-      }, 
-      locInfoDto: {
-          id: ''
-          
-      },
-      vendorInfoDto:{
-      id:''
-      },
-      employeeDto: {
-          id: '' 
-      }
-  });
+  const [formValues, setFormValues] = useState({
+    assetName : '' ,
+    description :  '',
+    make :  '' ,
+    model : '' ,
+    isSerialized : true,
+    value :'',
+     type : '' ,
+      level1 :  '' ,
+      level2 :  '' ,
+      level3 :  '' ,
+      level4 :  '' ,
+     serialNo :  '' ,
+     geoLocation :  '' , 
+     purchaseDate :  '' , 
+     isUnderWarranty : true,
+     warrantyEndDate : '' ,
+     amcendDate : '' ,
+     underAMC : true,
+     status :  '' ,
+     phoneNum : '' ,
+     isApprovedByHOD : false,
+     isApprovedByPrinicipal : false,
+     deptInfoDto :'',
+     locInfoDto : '',
+     vendorInfoDto : '',
+     employeeDto : ''
+});
   
 
   const [selectedImage, setSelectedImage] = useState(null);
   const { createAsset, snackbarOpen, snackbarMessage, snackbarSeverity, closeSnackbar } = useAsset();
   const { departments = [], getDepts } = useDept();
-  const context= useContext(EmployeeContext);
-  const {employees=[],getEmployees}=context;
+  const { locations = [], getLoctns } = useLoctn();
+  const { vendors = [], getVendors } = useVendor();
+
+  const [employees, setEmployees]=useState([]);
+  const [listitems, setlistitems]= useState([]);
+  const {getSysList,syslist }= useList();
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -57,10 +63,49 @@ const AssetCreate = () => {
     });
   };
 
-  useEffect(() => {
-    getDepts();
-    getEmployees();
-  }, []);
+  const assetData = {
+    ...formValues,
+  
+  };
+  const baseURl= process.env.REACT_APP_API_KEY
+const getListItems = async () => {
+  try {
+    const response = await axios.get(`${baseURl}/list/item-list/${assetData.type}`, {
+    });
+    setlistitems(response.data.data);
+  } catch (error) {
+    console.error("Error fetching vendors data:", error);
+  }
+};
+
+const getEmployees = async () => {
+  try {
+    console.log(assetData.deptInfoDto)
+      const response = await axios.get(`${baseURl}/employees/department/${assetData.deptInfoDto}`);
+      setEmployees(response.data);
+      console.log(response.data);
+  } catch (error) {
+      console.error("Error fetching employees data:", error);
+  }
+};
+
+      useEffect(() => {
+        getEmployees();
+      }, [assetData.deptInfoDto]); 
+
+     useEffect(() => {
+      getDepts();
+      getSysList();
+      getLoctns();
+      }, []);
+
+     useEffect(() => {
+      getListItems();
+      }, [assetData.type]);
+
+      useEffect(() => {
+        getVendors();
+    }, []);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -70,39 +115,42 @@ const AssetCreate = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const assetData = {
-      ...formValues,
-    
-    };
+ 
 const asset={
-    assetName:assetData.assetName ,
-      make: assetData.make,
-      model: assetData.model,
-      isSerialized: assetData.isSerialized,
-      serialNo:assetData.serialNo,
-      description: assetData.description,
-      geoLocation: assetData.geoLocation, 
-      purchaseDate: assetData.purchaseDate, 
-      warranty: assetData.warranty,
-      isWarrantyExpired: assetData.isWarrantyExpired,
-      isUnderWarranty: assetData.isUnderWarranty,
-      status: assetData.status,
-      isApprovedByHOD: assetData.isApprovedByHOD,
-      isApprovedByPrinicipal: assetData.isApprovedByPrinicipal,
-      deptInfoDto: {
-          id: ''
-          
-      }, 
-      locInfoDto: {
-          id: ''
-          
-      },
-      vendorInfoDto:{
-      id:''
-      },
-      employeeDto: {
-          id: '' 
-      }
+  assetName : assetData.assetName ,
+    description :  assetData.description,
+    make :  assetData.make ,
+    model : assetData.model ,
+    isSerialized : assetData.isSerialized,
+    value :assetData.value,
+     type : assetData.type ,
+      level1 :  assetData.level1 || "" ,
+      level2 :  assetData.level1 || "" ,
+      level3 :  assetData.level3 || "" ,
+      level4 :  assetData.level4 || "" ,
+     serialNo :  assetData.serialNo ,
+     geoLocation :  assetData.geoLocation || "" , 
+     purchaseDate :  assetData.purchaseDate , 
+     isUnderWarranty : assetData.isUnderWarranty,
+     warrantyEndDate : assetData.warrantyEndDate ,
+     amcendDate : assetData.amcendDate,
+      underAMC : assetData.underAMC||"",
+     status :  assetData.status ,
+   phoneNum : assetData.phoneNum ,
+     isApprovedByHOD : false,
+     isApprovedByPrinicipal : false,
+     deptInfoDto : {
+         id : assetData.deptInfoDto
+    },
+     locInfoDto : {
+         id : assetData.locInfoDto
+    },
+     vendorInfoDto : {
+         id : assetData.vendorInfoDto
+    },
+     employeeDto : {
+         id : assetData.employeeDto
+  }
     }
     createAsset(asset, navigate);
   };
@@ -162,7 +210,7 @@ const asset={
             />
           </div>
 
-          {/* <div className="flex w-2/3 justify-between items-center">
+          <div className="flex w-2/3 justify-between items-center">
             <FormLabel className="font-bold text-black w-1/3">
               Type <span className="text-red-600">*</span>
             </FormLabel>
@@ -175,8 +223,9 @@ const asset={
                   onChange={handleChange}
                   className="w-full"
                 >
-                  <MenuItem value="Type1">Type 1</MenuItem>
-                  <MenuItem value="Type2">Type 2</MenuItem>
+               {syslist.map(syslist => (
+                                        <MenuItem value={`${syslist.name} `}>{syslist.name}</MenuItem>
+                                    ))}
                 </Select>
               </FormControl>
             </Box>
@@ -190,17 +239,18 @@ const asset={
               <FormControl fullWidth>
                 <InputLabel>Sub-Type</InputLabel>
                 <Select
-                  name="subType"
-                  value={formValues.subType}
+                  name="level1"
+                  value={formValues.level1}
                   onChange={handleChange}
                   className="w-full"
                 >
-                  <MenuItem value="SubType1">Sub-Type 1</MenuItem>
-                  <MenuItem value="SubType2">Sub-Type 2</MenuItem>
+                  {Array.isArray(listitems) && listitems.map(listitem => (
+                                        <MenuItem value={`${listitem.listItem} `}>{listitem.listItem}</MenuItem>
+                                    ))}
                 </Select>
               </FormControl>
             </Box>
-          </div> */}
+          </div>
 
           <div className="flex w-2/3 justify-between items-center">
             <FormLabel className="font-bold text-black w-1/3">
@@ -254,7 +304,7 @@ const asset={
                     <Box className="w-full bg-white mb-3 rounded-md">
                         <FormControl fullWidth>
                     <InputLabel id="demo-simple-select-label">Status</InputLabel>
-                            <Select
+                            <Select 
                                 labelId="demo-simple-select-label"
                                 name='status'
                                 value={formValues.status}
@@ -269,47 +319,7 @@ const asset={
                         </FormControl>
                         </Box>
                     </div>
-
-          <div className="flex w-2/3 justify-between items-center">
-            <FormLabel className="font-bold text-black w-1/3">
-              Location <span className="text-red-600">*</span>
-            </FormLabel>
-            <TextField
-              fullWidth
-              label="Enter Location"
-              name="locInfoDto"
-              value={formValues.locInfoDto}
-              onChange={handleChange}
-              sx={{ mb: 2, backgroundColor: 'white' }}
-              className="w-full rounded-md"
-            />
-          </div>
-
-          <div className="flex w-2/3 justify-between items-center">
-            <FormLabel className="font-bold text-black w-1/3">
-            Incharge Name <span className="text-red-600">*</span>
-            </FormLabel>
-            <Box className="w-full bg-white mb-3 rounded-md">
-              <FormControl fullWidth>
-                <InputLabel>Incharge Name</InputLabel>
-                <Select
-                  name="assetInchargeDto"
-                  value={formValues.assetInchargeDto}
-                  onChange={handleChange}
-                  className="w-full"
-                >
-                  {employees.map(emp => (
-                    <MenuItem key={emp.id} value={emp.id}>
-                      {emp.name} 
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Box>
-          </div>
-
-
-          <div className="flex w-2/3 justify-between items-center">
+                    <div className="flex w-2/3 justify-between items-center">
             <FormLabel className="font-bold text-black w-1/3">
               Department <span className="text-red-600">*</span>
             </FormLabel>
@@ -317,8 +327,8 @@ const asset={
               <FormControl fullWidth>
                 <InputLabel>Department</InputLabel>
                 <Select
-                  name="userDept"
-                  value={formValues.userDept}
+                  name="deptInfoDto"
+                  value={formValues.deptInfoDto}
                   onChange={handleChange}
                   className="w-full"
                 >
@@ -331,6 +341,75 @@ const asset={
               </FormControl>
             </Box>
           </div>
+          <div className="flex w-2/3 justify-between items-center">
+            <FormLabel className="font-bold text-black w-1/3">
+            Incharge Name <span className="text-red-600">*</span>
+            </FormLabel>
+            <Box className="w-full bg-white mb-3 rounded-md">
+              <FormControl fullWidth>
+                <InputLabel>Incharge Name</InputLabel>
+                <Select
+                  name="employeeDto"
+                  value={formValues.employeeDto}
+                  onChange={handleChange}
+                  className="w-full"
+                >
+                  {employees.map(emp => (
+                    <MenuItem key={emp.id} value={emp.id}>
+                      {emp.name} 
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
+          <div className="flex w-2/3 justify-between items-center">
+            <FormLabel className="font-bold text-black w-1/3">
+              Location <span className="text-red-600">*</span>
+            </FormLabel>
+                <Box className="w-full bg-white mb-3 rounded-md">
+              <FormControl fullWidth>
+                <InputLabel>Location</InputLabel>
+                <Select
+                  name="locInfoDto"
+                  value={formValues.locInfoDto}
+                  onChange={handleChange}
+                  className="w-full"
+                >
+                  {locations.map(location => (
+                    <MenuItem key={location.id} value={location.id}>
+                      {location.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
+
+          <div className="flex w-2/3 justify-between items-center">
+            <FormLabel className="font-bold text-black w-1/3">
+              Location <span className="text-red-600">*</span>
+            </FormLabel>
+                <Box className="w-full bg-white mb-3 rounded-md">
+              <FormControl fullWidth>
+                <InputLabel>Location</InputLabel>
+                <Select
+                  name="vendorInfoDto"
+                  value={formValues.vendorInfoDto}
+                  onChange={handleChange}
+                  className="w-full"
+                >
+                  {vendors.map(vendor => (
+                    <MenuItem key={vendor.id} value={vendor.id}>
+                      {vendor.companyName}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
+
+
 
           <div className="flex w-2/3 justify-between items-center">
             <FormLabel className="font-bold text-black w-1/3">
@@ -353,8 +432,6 @@ const asset={
             </FormLabel>
             <TextField
               fullWidth
-              label="Enter Purchase Date"
-              name="purchaseDate"
               type="date"
               value={formValues.purchaseDate}
               onChange={handleChange}
@@ -392,7 +469,65 @@ const asset={
               className="w-full rounded-md"
             />
           </div>
+              <div className="flex w-2/3 items-center">
+                <FormLabel className="font-bold text-black w-1/3">
+                 Is Under Warranty <span className="text-red-600">*</span>
+                </FormLabel>
+                <Checkbox
+                  fullWidth
+                  name="isUnderWarranty"
+                  value={formValues.isUnderWarranty}
+                  onChange={handleChange}
+                  sx={{ mb: 2}}
+                  type="checkbox"
+                  className="rounded-md justify-start"
+                />
+              </div>
 
+              <div className="flex w-2/3 justify-between items-center">
+                <FormLabel className="font-bold text-black w-1/3">
+                  Warranty End Date <span className="text-red-600">*</span>
+                    </FormLabel>
+                    <TextField
+                      fullWidth
+     
+                      name="warrantyEndDate"
+                      type="date"
+                      value={formValues.warrantyEndDate}
+                      onChange={handleChange}
+                      sx={{ mb: 2, backgroundColor: 'white' }}
+                      className="w-full rounded-md"
+                    />
+                </div>
+                <div className="flex w-2/3 items-center">
+                <FormLabel className="font-bold text-black w-1/3">
+                 Is Under AMC <span className="text-red-600">*</span>
+                </FormLabel>
+                <Checkbox
+                  fullWidth
+                  name="underAMC"
+                  value={formValues.underAMC}
+                  onChange={handleChange}
+                  sx={{ mb: 2}}
+                  type="checkbox"
+                  className="rounded-md justify-start"
+                />
+              </div>
+
+              <div className="flex w-2/3 justify-between items-center">
+                <FormLabel className="font-bold text-black w-1/3">
+                  AMC End Date <span className="text-red-600">*</span>
+                    </FormLabel>
+                    <TextField
+                      fullWidth
+                      name="amcendDate"
+                      type="date"
+                      value={formValues.amcendDate}
+                      onChange={handleChange}
+                      sx={{ mb: 2, backgroundColor: 'white' }}
+                      className="w-full rounded-md"
+                    />
+                </div>
           <div className="flex w-2/3 justify-between items-center">
             <FormLabel className="font-bold text-black w-1/3">
               Asset Image <span className="text-red-600">*</span>
@@ -420,7 +555,7 @@ const asset={
             </div>
           )}
 
-          {/* Submit Button */}
+
           <Button
             type="submit"
             variant="contained"
